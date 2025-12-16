e360afc23317a5d192747968fd43247581602700
Venokos <2818645842@qq.com>
2025-12-16 16:29:51 +0800

    20251216-2

--- LinearList/SequenceList/test1.c
+++ LinearList/SequenceList/test1.c
@@ -5,9 +5,10 @@
 typedef int ElemType; // 方便后续修改元素数据类型 （高内聚 低耦合）
 
 typedef struct {
-    ElemType data;
+    ElemType data[MAX_SIZE];
     int length;
 }SeqList;
+
 // 初始化
 void initList(SeqList* list) {
     list->length = 0;
@@ -18,10 +19,71 @@ int appendElem(SeqList* L, ElemType e) {
         printf("The list is full!\n");
         return 0;
     }
-
+    L->data[L->length] = e;
+    L->length++;
+    return 1;
+}
+// 遍历输出顺序表内容
+void printList(SeqList* L) {
+    for (int i = 0; i < L->length; i++) {
+        printf("%d ", L->data[i]);
+    }
+    printf("\n");
+}
+// 插入元素 pos代表插入位置，不是下标
+int insertElem(SeqList *L, int pos, ElemType e) {
+    if (L->length >= MAX_SIZE) {
+        printf("The list is full!\n");
+        return 0;
+    }
+    if (pos < 1 || pos > L->length) {
+        printf("Wrong position!\n");
+        return 0;
+    }
+    for (int i = L->length - 1; i >= pos - 1; i--) {
+        L->data[i+1] = L->data[i];
+    }
+    L->data[pos-1] = e;
+    L->length++;
+    return 1;
+}
+// 删除元素 e用于存储删除的数据
+int deleteElem(SeqList *L, int pos, ElemType *e) {
+    if (L->length == 0) {
+        printf("Empty list!\n");
+        return 0;
+    }
+    if (pos < 1 || pos > L->length) {
+        printf("Wrong position!\n");
+        return 0;
+    }
+    *e = L->data[pos-1];
+    for (int i = pos - 1; i < L->length; i++) {
+        L->data[i] = L->data[i+1];
+    }
+    L->length--;
+    return 1;
 }
 
 int main() {
+    SeqList list;
+    initList(&list);
+    printf("初始化顺序表成功，当前顺序表长度:%d\n", list.length);
+    printf("顺序表内存占用:%zu\n", sizeof(list.data));
+    appendElem(&list, 40);
+    appendElem(&list, 42);
+    appendElem(&list, 46);
+    appendElem(&list, 57);
+    printList(&list);
+
+    insertElem(&list, 2, 88);
+    printList(&list);
+
+    ElemType deleted;
+    deleteElem(&list, 2, &deleted);
+    printList(&list);
+    printf("被删除数据:%d\n", deleted);
+
 
 
 
